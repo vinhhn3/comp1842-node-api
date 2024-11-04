22496b0cf22c86261221678a47566cbd09d1a639
Vinh Hoang <vinhhn3@fe.edu.vn>
2024-11-04 08:40:14 +0700

    update

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://vinhhn3:Teax7XHDcgcgNXuD@cluster0.hdjsj3r.mongodb.net/comp1842-node-api?retryWrites=true&w=majority&appName=Cluster0",
+      "mongodb+srv://vinhhn3:vqQBiIMHUUyXThV6@cluster0.hdjsj3r.mongodb.net/comp1842-node-api?retryWrites=true&w=majority&appName=Cluster0",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
